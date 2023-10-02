54e222ea90d8d8c2c6ac459113778c9dced4647a
Boris Labianca <boris.labianca@hotmail.fr>
2023-10-02 16:22:02 +0800

    created sign up login forgotten pwd and reset pwd pages

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
+import styles from "./Auth.module.scss";
+import { BiLogIn } from "react-icons/bi";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <BiLogIn size={35} color="#999" />
+          </div>
+          <h2>Login</h2>
+          <form>
+            <input type="email" placeholder="Email" required name="email" />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              name="password"
+            />
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Login
+            </button>
+          </form>
+          <Link to="/forgot">Forgot Password</Link>
+          <span className={styles.register}>
+            <Link to="/">Home</Link>
+            <p>&nbsp; Don't have an account? &nbsp;</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Auth/Reset.jsx
+++ src/pages/Auth/Reset.jsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
+import styles from "./Auth.module.scss";
+import { MdPassword } from "react-icons/md";
+
 const Reset = () => {
-  return <div>Reset</div>;
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <MdPassword size={35} color="#999" />
+          </div>
+          <h2>Forgot Password</h2>
+          <form>
+            <input
+              type="password"
+              placeholder="New Password"
+              required
+              name="newPassword"
+            />
+            <input
+              type="password"
+              placeholder="Confirm New Password"
+              required
+              name="confirmNewPassword"
+            />
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/">- Home</Link>
+              </p>
+              <p>
+                <Link to="/login">- Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Reset;

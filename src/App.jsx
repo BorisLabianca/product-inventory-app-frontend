7514e0b9c1ab2fb1a0171ad964cb2efcbbb3a807
Boris Labianca <boris.labianca@hotmail.fr>
2023-10-02 15:43:04 +0800

    added routes for login sign up reset pwd and forgotten pwd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Reset from "./pages/Auth/Reset";
+import Forgot from "./pages/Auth/Forgot";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/reset-password/:resetToken" element={<Reset />} />
       </Routes>
     </Router>
   );
